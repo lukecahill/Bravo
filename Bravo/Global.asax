e48808bf403b22a5b9e2ced629962e52aeda5a58
Luke Cahill <lmc@outlook.com>
2016-01-23 19:38:18 +0000

    Initial commit.

---
+++ Bravo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bravo.MvcApplication" Language="C#" %>
---
+++ Bravo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bravo.Startup))]
+namespace Bravo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
