57e3f121c6780643daad1138eafec0e506975cf2
Luke Cahill <lmc@outlook.com>
2016-01-23 20:01:47 +0000

    Added Bravo Context class Default configuration data Enable migrations

---
+++ Bravo/DAL/BravoContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System.Diagnostics;
+using model = Bravo.Models;
+
+namespace Bravo.DAL {
+	public class BravoContext : DbContext {
+		public BravoContext() : base("name=BravoContext") {
+			Database.SetInitializer<BravoContext>(null);
+			Configuration.LazyLoadingEnabled = false;
+
+			this.Database.Log = s => Debug.WriteLine(s);
+		}
+
+		public DbSet<model.Album> Albums { get; set; }
+		public DbSet<model.Artist> Artists { get; set; }
+		public DbSet<model.Genre> Genres { get; set; }
+		public DbSet<model.Song> Songs { get; set; }
+	}
+}
---
+++ Bravo/Migrations/Configuration.cs
@@ -0,0 +1,58 @@
+namespace Bravo.Migrations {
+	using System.Collections.Generic;
+	using System.Data.Entity.Migrations;
+	using System.Linq;
+	using model = Bravo.Models;
+
+	internal sealed class Configuration : DbMigrationsConfiguration<Bravo.DAL.BravoContext> {
+		public Configuration() {
+			AutomaticMigrationsEnabled = false;
+		}
+
+		protected override void Seed(Bravo.DAL.BravoContext context) {
+			context.Albums.AddOrUpdate(GetAlbums.ToArray());
+			context.Artists.AddOrUpdate(GetArtists.ToArray());
+			context.Genres.AddOrUpdate(GetGenres.ToArray());
+			context.Songs.AddOrUpdate(GetSongs.ToArray());
+		}
+
+		private IList<model.Album> GetAlbums {
+			get {
+				return new List<model.Album> {
+					new model.Album { AlbumId = 1, AlbumName = "Indestructible", ArtistId = 1, GenreId = 1 },
+					new model.Album { AlbumId = 2, AlbumName = "My Beautifiul Dark Twisted Fantasy", ArtistId = 2, GenreId = 2 },
+					new model.Album { AlbumId = 3, AlbumName = "Stadium Arcadium", ArtistId = 3, GenreId = 1 }
+				};
+			}
+		}
+
+		private IList<model.Artist> GetArtists {
+			get {
+				return new List<model.Artist> {
+					new model.Artist { ArtistId = 1, ArtistName = "Disturbed", AlbumId = 1 },
+					new model.Artist { ArtistId = 2, ArtistName = "Kanye West", AlbumId = 2 },
+					new model.Artist { ArtistId = 3, ArtistName = "Red Hot Chili Peppers", AlbumId = 3 }
+				};
+			}
+		}
+
+		private IList<model.Genre> GetGenres {
+			get {
+				return new List<model.Genre> {
+					new model.Genre { GenreId = 1, GenreName = "Rock" },
+					new model.Genre { GenreId = 2, GenreName = "Rap" }
+				};
+			}
+		}
+
+		private IList<model.Song> GetSongs {
+			get {
+				return new List<model.Song> {
+					new model.Song { SongId = 1, SongName = "Indestructible", AlbumId = 1 },
+					new model.Song { SongId = 2, SongName = "Deveil in a New Dress", AlbumId = 2 },
+					new model.Song { SongId = 3, SongName = "Otherside", AlbumId = 3 }
+			};
+			}
+		}
+	}
+}
