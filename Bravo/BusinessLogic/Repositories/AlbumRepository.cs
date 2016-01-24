8807da0b4f0c1d7967fdd515d43eb73d5421d41f
Luke Cahill <lmc@outlook.com>
2016-01-24 12:16:39 +0000

    Add ablum repository

---
+++ Bravo/BusinessLogic/Repositories/AlbumRepository.cs
@@ -0,0 +1,55 @@
+﻿using Bravo.DAL;
+using Bravo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Bravo.BusinessLogic.Repositories {
+	public class AlbumRepository : IDisposable {
+
+		private readonly BravoContext db = new BravoContext();
+
+		public IEnumerable<AlbumViewModel>GetAll() {
+			var entity = db.Albums.ToList();
+
+			return entity.Select(e => new AlbumViewModel(e)).ToList();
+		}
+
+		public AlbumViewModel GetById(int id) {
+			var entity = db.Albums.FirstOrDefault(e => e.AlbumId == id);
+
+			return new AlbumViewModel(entity);
+		}
+
+		public AlbumViewModel Create(Models.Album album) {
+			db.Albums.Add(album);
+			db.SaveChanges();
+
+			return new AlbumViewModel(album);
+		}
+
+		public void Update(Models.Album album) {
+			var entity = db.Albums.FirstOrDefault(e => e.AlbumId == album.AlbumId);
+
+			entity.AlbumId = album.AlbumId;
+			entity.AlbumName = album.AlbumName;
+			entity.ArtistId = album.ArtistId;
+			entity.GenreId = album.GenreId;
+
+			db.Entry(entity).State = EntityState.Modified;
+			db.SaveChanges();
+		}
+
+		public void Delete(int id) {
+			var entity = db.Albums.FirstOrDefault(e => e.AlbumId == id);
+
+			db.Albums.Remove(entity);
+			db.SaveChanges();
+		}
+
+		public void Dispose() {
+			db.Dispose();
+		}
+	}
+}
